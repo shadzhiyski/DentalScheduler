b4c77dfb48d78c3cadb5062228a6f79f31568418
stoian2662 <stoian2662@gmail.com>
2020-08-25 00:23:58 +0300

    Checking for invalid Password added.

--- DentalScheduler.UseCases/LoginCommand.cs
+++ DentalScheduler.UseCases/LoginCommand.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using DentalScheduler.DTO.Output;
 using DentalScheduler.DTO.Output.Common;
@@ -8,6 +9,7 @@ using DentalScheduler.Interfaces.Models.Output;
 using DentalScheduler.Interfaces.Models.Output.Common;
 using DentalScheduler.Interfaces.UseCases;
 using DentalScheduler.Interfaces.UseCases.Validation;
+using DentalScheduler.UseCases.Validation;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 
@@ -44,6 +46,19 @@ namespace DentalScheduler.UseCases
             }
 
             var user = await UserService.FindByNameAsync(userInput.UserName);
+            if (!(await UserService.CheckPasswordAsync(user, userInput.Password)))
+            {
+                validationResult.Errors.Add(
+                    new ValidationError()
+                    {
+                        PropertyName = nameof(IUserCredentialsInput.Password),
+                        Errors = new [] { "Invalid password." }
+                    }
+                );
+
+                return new Result<IAccessTokenOutput>(validationResult.Errors);
+            }
+
             var roleName = (await UserService.GetRolesAsync(user)).FirstOrDefault();
             var tokenString = JwtAuthManager.GenerateJwt(userInput, roleName);
 
