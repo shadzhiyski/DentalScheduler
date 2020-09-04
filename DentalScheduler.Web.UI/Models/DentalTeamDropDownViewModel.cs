f22d6b4c03a361918c6753e86506216f6c0e3c09
stoian2662 <stoian2662@gmail.com>
2020-09-04 14:08:14 +0300

    Dental teams changed to load from service and setting parameters extended.

---
+++ DentalScheduler.Web.UI/Models/DentalTeamDropDownViewModel.cs
@@ -0,0 +1,9 @@
+namespace DentalScheduler.Web.UI.Models
+{
+    public class DentalTeamDropDownViewModel
+    {
+        public string Name { get; set; }
+
+        public string ReferenceId { get; set; }
+    }
+}
