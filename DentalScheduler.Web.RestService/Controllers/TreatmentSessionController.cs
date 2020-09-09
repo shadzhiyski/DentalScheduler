c0fc5f3861b7b1e903a475154a6b505ab6f366d9
stoian2662 <stoian2662@gmail.com>
2020-09-09 17:21:58 +0300

    Add and Update commands for TreatmentSession applied.

--- DentalScheduler.Web.RestService/Controllers/TreatmentSessionController.cs
+++ DentalScheduler.Web.RestService/Controllers/TreatmentSessionController.cs
@@ -1,8 +1,10 @@
 using System.Linq;
 using System.Threading.Tasks;
+using DentalScheduler.DTO.Input;
 using DentalScheduler.DTO.Output;
 using DentalScheduler.Entities;
 using DentalScheduler.Interfaces.Gateways;
+using DentalScheduler.Interfaces.UseCases;
 using Mapster;
 using Microsoft.AspNet.OData;
 using Microsoft.AspNet.OData.Query;
@@ -18,15 +20,23 @@ namespace DentalScheduler.Web.RestService.Controllers
     {
         public TreatmentSessionController(
             TypeAdapterConfig mappingConfig,
-            IGenericRepository<TreatmentSession> repository)
+            IGenericRepository<TreatmentSession> repository,
+            IAddTreatmentSessionCommand addTreatmentSessionCommand,
+            IUpdateTreatmentSessionCommand updateTreatmentSessionCommand)
         {
             MappingConfig = mappingConfig;
             Repository = repository;
+            AddTreatmentSessionCommand = addTreatmentSessionCommand;
+            UpdateTreatmentSessionCommand = updateTreatmentSessionCommand;
         }
 
         public TypeAdapterConfig MappingConfig { get; }
 
         public IGenericRepository<TreatmentSession> Repository { get; }
+
+        public IAddTreatmentSessionCommand AddTreatmentSessionCommand { get; }
+
+        public IUpdateTreatmentSessionCommand UpdateTreatmentSessionCommand { get; }
 
         [HttpGet]
         [EnableQuery(AllowedQueryOptions = AllowedQueryOptions.All)]
@@ -34,6 +44,22 @@ namespace DentalScheduler.Web.RestService.Controllers
         {
             return options.ApplyTo(Repository.AsQueryable())
                 .ProjectToType<TreatmentSessionOutput>(MappingConfig);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostAsync(TreatmentSessionInput input)
+        {
+            var result = await AddTreatmentSessionCommand.ExecuteAsync(input);
+
+            return PresentResult(result);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> PutAsync(TreatmentSessionInput input)
+        {
+            var result = await UpdateTreatmentSessionCommand.ExecuteAsync(input);
+
+            return PresentResult(result);
         }
     }
 }
