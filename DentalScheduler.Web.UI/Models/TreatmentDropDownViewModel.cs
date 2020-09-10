6cd09c03d1fef77790a948ed74a19067a4df20f9
stoian2662 <stoian2662@gmail.com>
2020-09-10 21:52:06 +0300

    TreatmentDropDownViewModel created.

---
+++ DentalScheduler.Web.UI/Models/TreatmentDropDownViewModel.cs
@@ -0,0 +1,11 @@
+namespace DentalScheduler.Web.UI.Models
+{
+    public class TreatmentDropDownViewModel
+    {
+        public string ReferenceId { get; set; }
+
+        public string Name { get; set; }
+
+        public int DurationInMinutes { get; set; }
+    }
+}
