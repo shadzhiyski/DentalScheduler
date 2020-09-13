ba94105f97177bbde6f0210129d99b7a44680daa
stoian2662 <stoian2662@gmail.com>
2020-09-14 00:11:34 +0300

    Usings clean up.

--- DentalScheduler.Web.UI/Services/TreatmentService.cs
+++ DentalScheduler.Web.UI/Services/TreatmentService.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
-using System.Net.Http.Json;
 using System.Threading.Tasks;
 using Blazored.LocalStorage;
 using DentalScheduler.Web.UI.Models;
