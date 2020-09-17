52ec7e15bc44e9d0c76fba57cc062ab5a88494e3
stoian2662 <stoian2662@gmail.com>
2020-09-17 23:21:19 +0300

    FakeLogger created.

---
+++ DentalScheduler.UseCases.Tests/Utilities/Fakes/FakeLogger.cs
@@ -0,0 +1,23 @@
+using System;
+using Microsoft.Extensions.Logging;
+
+namespace DentalScheduler.UseCases.Tests.Fakes
+{
+    public class FakeLogger<T> : ILogger<T>
+    {
+        public IDisposable BeginScope<TState>(TState state)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            return true;
+        }
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            
+        }
+    }
+}
