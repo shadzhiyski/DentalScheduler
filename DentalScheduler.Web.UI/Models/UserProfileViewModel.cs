4d7847d0be879928189c2c360cf9ae28bd453971
stoian2662 <stoian2662@gmail.com>
2020-09-24 15:16:20 +0300

    UserProfileViewModel created.

---
+++ DentalScheduler.Web.UI/Models/UserProfileViewModel.cs
@@ -0,0 +1,11 @@
+namespace DentalScheduler.Web.UI.Models
+{
+    public class UserProfileViewModel
+    {
+        public byte[] Avatar { get; set; }
+        
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
