db3aef259028e6d2e5c58ecd13eaf5aada2220e3
stoian2662 <stoian2662@gmail.com>
2021-03-22 09:58:58 +0200

    treatments module added to vuex store.

---
+++ DentalSystem.Web.UI/src/scheduling/store/treatments.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+const state = {
+  treatments: []
+};
+
+const getters = {
+  allTreatments: (state) => state.treatments,
+};
+
+const actions = {
+  async getTreatments({commit}) {
+    let response = await axios.get("odata/Treatment");
+    await commit("setTreatments", response.data.value);
+  },
+};
+
+const mutations = {
+  setTreatments(state, treatments) {
+    state.treatments = treatments;
+  },
+};
+
+export default {
+  state,
+  getters,
+  actions,
+  mutations,
+};
