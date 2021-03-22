50f9be017cb78cba988400c9554d992299949647
stoian2662 <stoian2662@gmail.com>
2021-03-22 09:31:42 +0200

    Vuex lib added.

--- DentalSystem.Web.UI/src/main.js
+++ DentalSystem.Web.UI/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './common/router'
 import vuetify from './common/plugins/vuetify';
+import store from './common/store'
 import './common/plugins/axios'
 
 Vue.config.productionTip = false
@@ -9,5 +10,6 @@ Vue.config.productionTip = false
 new Vue({
   router,
   vuetify,
+  store,
   render: h => h(App)
 }).$mount('#app')
