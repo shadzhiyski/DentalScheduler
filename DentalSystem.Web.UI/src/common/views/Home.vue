8d9c3d61c0507a3bfd0c83d10fd5176c5d02ca93
stoian2662 <stoian2662@gmail.com>
2021-03-22 10:00:47 +0200

    HelloWorld replaced by Carousel in Home page.

--- DentalSystem.Web.UI/src/common/views/Home.vue
+++ DentalSystem.Web.UI/src/common/views/Home.vue
@@ -1,17 +1,40 @@
 <template>
   <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-row class="text-center">
+    <v-col class="mb-4">
+      <h1 class="display-2 font-weight-bold mb-3">
+        Welcome to Dental System
+      </h1>
+
+      <p class="subheading font-weight-regular">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+    </v-col>
+    </v-row>
+    <Carousel :slides="allTreatments.map(t => t.Name)" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/common/components/HelloWorld.vue'
+import Carousel from '@/common/components/Carousel.vue'
+import { mapGetters, mapActions } from "vuex";
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Carousel
+  },
+  computed: {
+    ...mapGetters([
+      'allTreatments',
+    ]),
+  },
+  methods: {
+    ...mapActions(["getTreatments"])
+  },
+  created() {
+    this.getTreatments();
   }
 }
 </script>
