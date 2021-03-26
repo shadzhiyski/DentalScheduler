3a347de25143c37bb437c3638cda485203b8eb3b
stoian2662 <stoian2662@gmail.com>
2021-03-26 19:40:55 +0200

    Common formStyle extracted.

--- DentalSystem.Web.UI/src/identity/views/UserProfile.vue
+++ DentalSystem.Web.UI/src/identity/views/UserProfile.vue
@@ -92,31 +92,4 @@ export default {
 </script>
 
 
-<style scoped>
-* {
-  box-sizing: border-box;
-}
-label {
-  padding: 12px 12px 12px 0;
-  display: inline-block;
-}
-button[type="submit"] {
-  background-color: #4caf50;
-  color: white;
-  padding: 12px 20px;
-  cursor: pointer;
-  border-radius: 30px;
-}
-button[type="submit"]:hover {
-  background-color: #45a049;
-}
-input {
-  margin: 5px;
-  box-shadow: 0 0 15px 4px rgba(0, 0, 0, 0.06);
-  padding: 10px;
-  border-radius: 30px;
-}
-#error {
-  color: red;
-}
-</style>
+<style scoped src="../../common/styles/formStyle.css"></style>
