a127ec8ab607f7f09207f1b144460fb868da893f
stoian2662 <stoian2662@gmail.com>
2021-03-27 10:29:22 +0200

    clean up.

--- DentalSystem.Web.UI/src/scheduling/components/EditTreatmentSession.vue
+++ DentalSystem.Web.UI/src/scheduling/components/EditTreatmentSession.vue
@@ -8,17 +8,8 @@
       :color="selectedEvent.color"
       dark
     >
-      <v-btn icon>
-      <v-icon>mdi-pencil</v-icon>
-      </v-btn>
       <v-toolbar-title v-html="selectedEvent.name"></v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn icon>
-      <v-icon>mdi-heart</v-icon>
-      </v-btn>
-      <v-btn icon>
-      <v-icon>mdi-dots-vertical</v-icon>
-      </v-btn>
     </v-toolbar>
     <form @submit.prevent="submit">
       <v-select
