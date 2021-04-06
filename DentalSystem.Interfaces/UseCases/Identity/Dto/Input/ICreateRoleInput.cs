4e75da2465dfbee9a429a6350be0cf2ae3732e42
stoian2662 <stoian2662@gmail.com>
2021-04-06 20:33:45 +0300

    Creating role and link user with role end points removed.

--- DentalSystem.Interfaces/UseCases/Identity/Dto/Input/ICreateRoleInput.cs
+++
@@ -1,7 +0,0 @@
-namespace DentalSystem.Interfaces.UseCases.Identity.Dto.Input
-{
-    public interface ICreateRoleInput
-    {
-        string Name { get; set; }
-    }
-}
--- DentalSystem.UseCases/Identity/Dto/Input/CreateRoleInput.cs
+++
@@ -1,9 +0,0 @@
-using DentalSystem.Interfaces.UseCases.Identity.Dto.Input;
-
-namespace DentalSystem.UseCases.Identity.Dto.Input
-{
-    public class CreateRoleInput : ICreateRoleInput
-    {
-        public string Name { get; set; }
-    }
-}
