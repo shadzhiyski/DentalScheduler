d3a80796233a4816c606a8a6c487b04d23a7d864
stoian2662 <stoian2662@gmail.com>
2021-04-12 20:34:27 +0300

    Failing integration tests fixed.

--- DentalSystem.Infrastructure/Common/Persistence/DentalSystemDbContext.cs
+++ DentalSystem.Infrastructure/Common/Persistence/DentalSystemDbContext.cs
@@ -3,14 +3,21 @@ using DentalSystem.Infrastructure.Scheduling.Persistence.Configurations;
 using DentalSystem.Entities.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace DentalSystem.Infrastructure.Common.Persistence
 {
     public class DentalSystemDbContext : IdentityDbContext<User>
     {
-        public DentalSystemDbContext(DbContextOptions<DentalSystemDbContext> options)
+        public DentalSystemDbContext(
+            DbContextOptions<DentalSystemDbContext> options,
+            Action<ModelBuilder> onModelCreated = null)
             : base(options)
-        { }
+        {
+            OnModelCreated = onModelCreated;
+        }
+
+        public Action<ModelBuilder> OnModelCreated { get; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
             => optionsBuilder
@@ -28,6 +35,8 @@ namespace DentalSystem.Infrastructure.Common.Persistence
             modelBuilder.ApplyConfiguration(new TreatmentSessionConfiguration());
             modelBuilder.ApplyConfiguration(new DentalTeamConfiguration());
             modelBuilder.ApplyConfiguration(new DentalTeamParticipantConfiguration());
+
+            OnModelCreated?.Invoke(modelBuilder);
         }
     }
 }
