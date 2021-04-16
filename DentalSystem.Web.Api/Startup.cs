7b674e61f3371f28ab6aca9600dd2404cf6b3b60
stoian2662 <stoian2662@gmail.com>
2021-04-16 23:11:02 +0300

    Clean-up and refactoring.

--- DentalSystem.Web.Api/Startup.cs
+++ DentalSystem.Web.Api/Startup.cs
@@ -1,15 +1,12 @@
 using System;
 using DentalSystem.Config.DI.Infrastructure;
 using DentalSystem.UseCases;
-using DentalSystem.UseCases.Scheduling.Dto.Output;
 using DentalSystem.Web.Api.Middleware;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.OData.Edm;
-using Microsoft.OData.ModelBuilder;
 
 namespace DentalSystem.Web.Api
 {
@@ -23,22 +20,15 @@ namespace DentalSystem.Web.Api
         public IConfiguration Configuration { get; }
 
         public void ConfigureServices(IServiceCollection services)
-        {
-            services
+            => services
                 .AddInfrastructure(Configuration)
-                .AddUseCases();
-
-            services.AddLocalizationMiddleware();
-
-            services.AddTransient(typeof(Lazy<>), typeof(Lazy<>));
-            services.AddControllers();
-
-            services.AddAuthorization();
-
-            services.AddODataMiddleware();
-
-            services.AddSwaggerMiddleware();
-        }
+                .AddUseCases()
+                .AddTransient(typeof(Lazy<>), typeof(Lazy<>))
+                .AddAuthorization()
+                .AddLocalizationMiddleware()
+                .AddODataMiddleware()
+                .AddSwaggerMiddleware()
+                .AddControllers();
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
@@ -47,47 +37,18 @@ namespace DentalSystem.Web.Api
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Initialize();
-
-            app.UseLocalizationMiddleware();
-
-            app.UseHttpsRedirection();
-
-            app.UseSwaggerMiddleware();
-
-            app.UseCorsMiddleware();
-
-            app.UseRouting();
-
-            app.UseAuthentication();
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-        }
-
-        private IEdmModel GetEdmModel()
-        {
-            ODataModelBuilder odataBuilder = new ODataConventionModelBuilder();
-
-            odataBuilder.EntitySet<RoomOutput>("Room")
-                .EntityType.HasKey(e => e.ReferenceId);
-
-            odataBuilder.EntitySet<DentalTeamOutput>("DentalTeam")
-                .EntityType.HasKey(e => e.ReferenceId);
-
-            var ts = odataBuilder.EntitySet<TreatmentSessionOutput>("TreatmentSession")
-                .EntityType.HasKey(e => e.ReferenceId);
-
-            odataBuilder.EntitySet<TreatmentOutput>("Treatment")
-                .EntityType.HasKey(e => e.ReferenceId);
-
-            odataBuilder.EntitySet<PatientOutput>("Patient")
-                .EntityType.HasKey(e => e.ReferenceId);
-
-            return odataBuilder.GetEdmModel();
+            app.Initialize()
+                .UseHttpsRedirection()
+                .UseRouting()
+                .UseAuthentication()
+                .UseAuthorization()
+                .UseLocalizationMiddleware()
+                .UseSwaggerMiddleware()
+                .UseCorsMiddleware()
+                .UseEndpoints(endpoints =>
+                {
+                    endpoints.MapControllers();
+                });
         }
     }
 }
